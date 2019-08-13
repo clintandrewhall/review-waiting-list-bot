70a54cef6731f010f7262bc1cd602514982ca613
Jean-Louis Leysens <jloleysens@gmail.com>
2019-08-13 22:01:55 +0200

    Properly encode labels with white space for parsing by Github

--- test/Condition.test.js
+++ test/Condition.test.js
@@ -20,4 +20,12 @@ describe('.toQuery', () => {
     const condition = new Condition('author', ['cat', 'dog'], false)
     expect(condition.toQuery()).toEqual('-author:cat -author:dog')
   })
+
+  test('properly encodes white spaces', () => {
+    const condition = new Condition('label', ['Team:With a space in it', 'Something with spaces'], false)
+    expect(condition.toQuery()).toEqual('-label:%22Team%3AWith%20a%20space%20in%20it%22 -label:%22Something%20with%20spaces%22')
+
+    const condition2 = new Condition('author', ['Friedrich Nietzsche', 'Spongebob Squarepants'], true)
+    expect(condition2.toQuery()).toEqual('author:%22Friedrich%20Nietzsche%22 author:%22Spongebob%20Squarepants%22')
+  })
 })
